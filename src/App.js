f25e6fcb16435fe93a92fd4784f5f80d75e96ad9
kamlendr <kamlend11@gmail.com>
2023-04-29 13:56:54 +0530

    user form styling fixed, css classes added

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    // width: 400,
     bgcolor: 'background.paper',
     // border: '2px solid #000',
     boxShadow: 24,
