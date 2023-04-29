94913832104d629b45445532ac4df50ddeaaaf68
kamlendr <kamlend11@gmail.com>
2023-04-29 12:24:24 +0530

    style and layout setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import './App.scss';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
+import GroupsIcon from '@mui/icons-material/Groups';
+import Button from '@mui/material/Button';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <div  >
+          <h3 style={{ textAlign: "center" }} >
+            Users
+          </h3>
+          <div>
+            user 1
+          </div>
+          <div>
+            user 2
+          </div>
+          <div>
+            user 3
+          </div>
+          <div>
+            user 4
+          </div>
+          <div>
+            user 5
+          </div>
+          <div>
+            user 6
+          </div>
+
+        </div>
+        <div  >
+          <h3 style={{ textAlign: "center" }} >
+            Rooms
+          </h3>
+        </div>
+        <div>
+          <Button variant='outlined' startIcon={<PersonAddIcon />}>
+            Add New User
+          </Button>
+          <Button variant='contained' startIcon={<GroupsIcon />}>
+            Schedule a meeting
+          </Button>
+          <Button variant='outlined' startIcon={<MeetingRoomIcon />}>
+            Add New Room
+          </Button>
+        </div>
+      </div>
+    </>
   );
 }
 
